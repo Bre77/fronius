482b8a1b131d848563a58a69c07b165c2efc418a
Brett <brett.whynot@gmail.com>
2024-09-12 11:33:25 +1000

    Fix build

--- .build.sh
+++ .build.sh
@@ -3,6 +3,6 @@ cd "${0%/*}"
 OUTPUT="${1:-fronius.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
-chmod -R u=rwx,go= *
+chmod -R u=rwx,go= bin/*
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --overwrite fronius 
